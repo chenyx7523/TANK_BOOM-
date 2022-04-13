1912a83b2041d0b8b7fbdb37d9833eafaed5a167
chenyx <664280120@qq.com>
2022-04-13 08:13:02 +0800

    star场景和脚本的搭建修复版

---
+++ Assets/Scripts/ScenesManager/ScenesManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    public void GameStar()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+
+
+}
