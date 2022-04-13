e46b8b2d810c27b37353b5280d4fc2bc38a6acbb
chenyx <664280120@qq.com>
2022-04-13 23:36:58 +0800

    血量的注释和锁定移动

---
+++ Assets/Scripts/Tank/TankHealth.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TankHealth : MonoBehaviour
+{
+
+    public float m_TankStarHealth=100f;            //  每个坦克开始时的生命值。
+    public Slider m_TankHealthSlider;              // 表示坦克当前生命值的滑块。
+    public Image m_TankHealthFillImage;                // 滑动块的图像组件。  
+    public Color m_FullHealthColor;                // 当生命值满时，生命条的颜色。
+    public Color m_ZeroHealthColor;                // 当没有生命值时，生命条的颜色
+    public GameObject m_TankDeathPrefab;           // 一个在Awake中实例化的预制件，当坦克b死亡时使用。
+
+
+    private AudioSource m_TankDeathAudio;            // 当坦克爆炸时播放的音频源。
+    private ParticleSystem m_TankDeathParticle;     // 坦克爆炸的粒子特效。 
+    private float m_TankCurrentHealth;              // 坦克当前生命值。
+    private bool m_TankIsDead;                      // 判断坦克是否死亡 
+
+
+    private void Awake()
+    {
+        //实例化爆炸预制件，并在其上获得粒子系统的参考。 
+        m_TankDeathParticle = Instantiate (m_TankDeathPrefab).GetComponent<ParticleSystem>();   //TODO
+
+        //获取实例化预制件(爆炸的粒子特效)上的音频源的引用。 
+        m_TankDeathAudio = m_TankDeathParticle.GetComponent<AudioSource>();
+
+        //禁用预制件，这样它就可以在需要的时候被激活。 
+        m_TankDeathParticle.gameObject.SetActive(false);    
+
+    }
+
+
+
+    private void OnEnable()
+    {   
+        // 当被启用时，重置坦克的健康状况以及死亡状态。
+        m_TankCurrentHealth = m_TankStarHealth;
+        m_TankIsDead = false;
+
+        // 更新运行状况滑块的值和颜色。
+        UpdateHealthUI();
+    }
+
+    //坦克受伤
+    public void TannkDamage (float amount)        //amount 即为收到的伤害值
+    {
+
+        // 根据造成的伤害减少当前生命值。
+        m_TankCurrentHealth -= amount;
+
+        // 更新运行状况滑块的值和颜色。
+        UpdateHealthUI();
+
+        // 如果当前运行状况为零或低于零且坦克已经死亡，则调用TankDeath方法。 
+        if (m_TankCurrentHealth <= 0f && m_TankIsDead)
+        {
+            TankDeath();
+        }
+
+
+
+    }
+
+
+
+
+
+    // 更新运行状况滑块的值和颜色。
+    private void UpdateHealthUI()
+    {
+        // 将当前声明的值赋值给UI滑块。
+        m_TankHealthSlider.value = m_TankCurrentHealth;
+        m_TankHealthSlider.maxValue = m_TankStarHealth;
+
+        //根据当前血量和满血的百分比，在选定的颜色之间插入条的颜色。
+        m_TankHealthFillImage.color = 
+            Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_TankCurrentHealth / m_TankStarHealth);
+
+
+
+    }
+
+
+
+    private void TankDeath()
+    {
+        // 设置该标志，以便此函数只被调用一次。
+        m_TankIsDead = true;
+        // 移动爆炸预制件到坦克的位置并生效。
+        m_TankDeathParticle.transform.position = transform.position;
+        m_TankDeathParticle.gameObject.SetActive(true);
+
+        // 播放坦克爆炸的粒子特效和音频。
+        m_TankDeathParticle.Play();
+        m_TankDeathAudio.Play();    
+
+        gameObject.SetActive(false);    
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/UI/UIHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHealth : MonoBehaviour
+{
+    // 确保世界空间UI生命值条等元素朝向正确的方向
+
+    public bool m_UseRelativeRotation = true;            // 这个游戏对象是否使用相对旋转
+
+    //m_localRotation 相对于父级变换旋转的变换旋转
+    private Quaternion m_localRotation;               //定义一个四元数，用来相对旋转   现场开始时的局部旋转。
+
+    private void Start()
+    {
+        //令四元数等于最初的旋转
+        m_localRotation = transform.localRotation;   //localRotation 用于相对旋转，限于四元数  https://docs.unity.cn/cn/2019.4/ScriptReference/Transform-localRotation.html
+    }
+
+    private void Update()
+    {
+        //不断使得相对旋转角度相同，即角度不变
+        if (m_UseRelativeRotation)
+            transform.rotation = m_localRotation;  //rotation 旋转属性
+    }
+}
